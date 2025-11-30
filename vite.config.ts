2ce448710242c56bafafc378c74ff253446a2238
Siddhinath Chakraborty <cseai2024003@rcciit.org.in>
2025-11-30 21:40:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // ❌ REMOVE base — Vercel breaks with './'
-  // base: '/vite-react/'  // DON'T USE THIS
-  // base: './',            // DON'T USE THIS
-
-  server: {
-    port: 5173,
-    open: true
-  },
+  // REQUIRED for Vercel to load assets correctly
+  base: '',
 
   build: {
     outDir: 'dist',
