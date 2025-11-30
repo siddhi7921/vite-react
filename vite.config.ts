0612a04fb054a561daa4bc6bc457f15f97b63566
Siddhinath Chakraborty <cseai2024003@rcciit.org.in>
2025-11-30 21:54:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Correct base for Vercel (root domain)
-  base: '/',
+  // Correct for Vercel (NEVER use ./ or /repo-name)
+  base: "/",
 
   build: {
-    outDir: 'dist',
-    sourcemap: false
+    outDir: "dist",
   }
 })
